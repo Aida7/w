676cbb364b16e816b4220c52f6af552e6b4e3fd9
Садыкова Аида Хаирбекова <@corp.itstep>
2018-04-19 12:11:16 +0600

    map

--- Maps/Program.cs
+++ Maps/Program.cs
@@ -10,6 +10,21 @@ namespace Maps
     {
         static void Main(string[] args)
         {
+            using (var context = new UsersContext())
+            {
+                context.Users.Add(new Users { Name = "Alice", Telephone=870165, Purse= 2000, Address = "the best",City="astana" ,Id=1});
+                context.Orders.Add(new Orders
+                {
+                    DeliveryTime = DateTime.Now,
+                    Price = 12121,
+                    TimeofOrder=DateTime.Now,
+                    UserId=1,
+                    TypeMachinesId=1,
+                    Id=1,
+                    DriverId=1,
+                    Route=1,
+                });
+            }
         }
     }
 }
